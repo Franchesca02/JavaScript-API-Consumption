1675bf84a369548002751e57f4988bd29ac54a88
Franchesca <ihuoma.franchesca@gmail.com>
2022-12-07 17:31:20 +0100

    added sections and footer

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ fetch("https://fakestoreapi.com/products/")
     allData.map((responses) =>{
         result+= `<div class="card">
         <a href="#"><h1 class="text-1xl text-yellow-700 font-bold underline">${responses.title}</h1></a>
-        <img src=${responses.image} alt="product images" class="image" />
+        <a href="#"><img src=${responses.image} alt="product images" class="image" /></a>
         <p class="description">${responses.description}</p>
         <p class="category">${responses.category}</p>
         <p class="price">${responses.price}</p>
